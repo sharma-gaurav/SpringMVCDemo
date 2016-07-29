ea437f84fcd420d52dc785f6ac7ceaaaafbec3b0
pulkit <pulkit.pushkarna@gmail.com>
2016-07-29 20:13:39 +0530

    DummyController added for testing

---
+++ src/main/java/com/spring/demo/controller/DummyController.java
@@ -0,0 +1,15 @@
+package com.spring.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DummyController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String index(){
+        return "Welcome to Spring Advance...";
+    }
+}
