78f770c2874b92c3e3bb2b9c2f0d9d4121b6fbed
gaurav-sharma <gaurav.sharma@tothenew.com>
2016-07-30 12:13:55 +0530

    Exercise 1.2

--- src/main/java/com/spring/demo/controller/StudentController.java
+++ src/main/java/com/spring/demo/controller/StudentController.java
@@ -2,15 +2,18 @@ package com.spring.demo.controller;
 
 
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class StudentController extends AbstractController {
-
-    @Override
-    protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
+public class StudentController extends MultiActionController {
+    public ModelAndView action1(HttpServletRequest request, HttpServletResponse response) {
         return new ModelAndView("index");
+    }
+
+    public void action2(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        response.setContentType("text/html");
+        response.getWriter().write("This is html content");
     }
 }
