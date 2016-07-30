7d7e368d6a469416379819f85ca55b8f9d5e9dc1
gaurav-sharma <gaurav.sharma@tothenew.com>
2016-07-30 14:42:49 +0530

    Exercise 5.1

--- src/main/java/com/spring/demo/co/StudentCO.java
+++ src/main/java/com/spring/demo/co/StudentCO.java
@@ -1,8 +1,17 @@
 package com.spring.demo.co;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+import java.util.List;
+
 public class StudentCO {
     String firstName;
     String lastName;
+    Long phoneNumber;
+    List<String> subjects;
+    @DateTimeFormat(pattern = "MM-dd-yyyy")
+    Date dateOfBirth;
 
     public String getFirstName() {
         return firstName;
@@ -18,5 +27,29 @@ public class StudentCO {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public Long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public List<String> getSubjects() {
+        return subjects;
+    }
+
+    public void setSubjects(List<String> subjects) {
+        this.subjects = subjects;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
     }
 }
