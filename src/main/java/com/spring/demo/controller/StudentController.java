135ef42f9de7b734b3ee129240cea8bc83c3bf1a
gaurav-sharma <gaurav.sharma@tothenew.com>
2016-07-30 15:57:06 +0530

    Exercise 6.1

--- src/main/java/com/spring/demo/controller/StudentController.java
+++ src/main/java/com/spring/demo/controller/StudentController.java
@@ -2,17 +2,27 @@ package com.spring.demo.controller;
 
 
 import com.spring.demo.co.StudentCO;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.DataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Map;
 
 @RequestMapping("/student")
 @Controller
 public class StudentController {
+
+    @InitBinder
+    void init(DataBinder dataBinder) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM-dd-yyyy");
+        dataBinder.registerCustomEditor(Date.class, "dateOfBirth", new CustomDateEditor(simpleDateFormat, true));
+    }
 
     @RequestMapping("/")
     ModelAndView index() {
@@ -40,7 +50,7 @@ public class StudentController {
     @RequestMapping(value = "/save")
     ModelAndView save(@ModelAttribute("student") StudentCO studentCO, BindingResult bindingResult) {
         ModelAndView modelAndView;
-        if(bindingResult.hasErrors()) {
+        if (bindingResult.hasErrors()) {
             modelAndView = new ModelAndView("student/create");
         } else {
             modelAndView = new ModelAndView("student/show");
