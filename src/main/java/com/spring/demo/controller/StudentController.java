cc3009bf0850b22470305a2804b30fad7d170ac3
gaurav-sharma <gaurav.sharma@tothenew.com>
2016-07-30 12:43:25 +0530

    Exercise 2.2 and 2.3.

--- src/main/java/com/spring/demo/controller/StudentController.java
+++ src/main/java/com/spring/demo/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.spring.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping("/student")
@@ -12,5 +13,18 @@ public class StudentController {
     @RequestMapping("/")
     ModelAndView index() {
         return new ModelAndView("index");
+    }
+
+    @RequestMapping("/sayHello")
+    @ResponseBody
+    String sayHello() {
+        return "Hello World";
+    }
+
+    @RequestMapping("/greet")
+    ModelAndView greet() {
+        ModelAndView modelAndView = new ModelAndView("greet");
+        modelAndView.addObject("greeting", "Hello World");
+        return modelAndView;
     }
 }
